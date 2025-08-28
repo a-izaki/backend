afccc9628f11dab94056888c9e196ffdea80b141
airton-izaki <airton.izaki@iesb.edu.br>
2025-08-28 10:46:31 -0300

    Arquivos aula 05 - Testes

---
+++ aulas/aula_04/index.test.js
@@ -0,0 +1,115 @@
+import {soma, subtracao, multiplicacao, divisao} from "./index.js";
+
+//.......Teste da Adição........
+if (soma(2,2) === 4 ) console.log("Passou 1º - Teste adição")
+else console.log ("Falhou 1º - Teste adição")
+
+
+if (soma(-1,2) === 1) console.log("Passou 2º - Teste adição")
+else console.log("Falhou 2º - Teste adição")
+
+if (soma(2,0) === 2) console.log("Passou 3º - Teste adição")
+else console.log("Falhou 3º - Teste adição") 
+
+
+//............Teste Subtração........
+console.log("Teste de Subtração")
+
+
+if (subtracao(3, -1) == 4) console.log("Passou 4º - teste subtração")
+else console.log("Falhou 4º - Tese subtração")
+
+if (subtracao(-3, -1) == -2) console.log("Passou 5º - teste subtração")
+    else console.log("Falhou 5º - Tese subtração")
+
+if (subtracao(3, 1) == 2) console.log("Passou 6º - teste subtração")
+    else console.log("Falhou 6º - Tese subtração")
+
+if (subtracao(-4, 1) == -5) console.log("Passou 7º - teste subtração")
+    else console.log("Falhou 7º - Tese subtração")
+
+if (subtracao(4, 0) == 4) console.log("Passou 8º - teste subtração")
+    else console.log("Falhou 8º - Tese subtração")
+
+if (subtracao(0, 1) == -1) console.log("Passou 9º - teste subtração")
+    else console.log("Falhou 9º - Tese subtração")
+
+
+//...............Teste Multiplicação..........
+console.log("Teste Multiplicação")
+
+if (multiplicacao(4, 2) === 8) console.log("Passou 11º - teste multiplicação")
+    else console.log("Falhou 11º - Tese subtração")
+
+if (multiplicacao(4, -2) === -8) console.log("Passou 12º - teste multiplicação")
+    else console.log("Falhou 12º - Tese subtração")
+
+if (multiplicacao(-4, 2) === -8) console.log("Passou 13º - teste multiplicação")
+    else console.log("Falhou 13º - Tese subtração")
+
+if (multiplicacao(4, 0) === 0) console.log("Passou 14º - teste multiplicação")
+    else console.log("Falhou 14º - Tese subtração")
+
+//...............Teste Multiplicação..........
+console.log("Teste Multiplicação")
+
+if (multiplicacao(4, 2) === 8) console.log("Passou 11º - teste multiplicação")
+    else console.log("Falhou 11º - Tese subtração")
+
+if (multiplicacao(4, -2) === -8) console.log("Passou 12º - teste multiplicação")
+    else console.log("Falhou 12º - Tese subtração")
+
+if (multiplicacao(-4, 2) === -8) console.log("Passou 13º - teste multiplicação")
+    else console.log("Falhou 13º - Tese subtração")
+
+if (multiplicacao(4, 0) === 0) console.log("Passou 14º - teste multiplicação")
+    else console.log("Falhou 14º - Tese subtração")
+
+//...............Teste Multiplicação..........
+console.log("Teste Multiplicação")
+
+if (multiplicacao(4, 2) === 8) console.log("Passou 11º - teste multiplicação")
+    else console.log("Falhou 11º - Tese subtração")
+
+if (multiplicacao(4, -2) === -8) console.log("Passou 12º - teste multiplicação")
+    else console.log("Falhou 12º - Tese subtração")
+
+if (multiplicacao(-4, 2) === -8) console.log("Passou 13º - teste multiplicação")
+    else console.log("Falhou 13º - Tese subtração")
+
+if (multiplicacao(4, 0) === 0) console.log("Passou 14º - teste multiplicação")
+    else console.log("Falhou 14º - Tese subtração")
+
+//...............Teste Multiplicação..........
+console.log("Teste Multiplicação")
+
+if (multiplicacao(4, 2) === 8) console.log("Passou 11º - teste multiplicação")
+    else console.log("Falhou 11º - Tese subtração")
+
+if (multiplicacao(4, -2) === -8) console.log("Passou 12º - teste multiplicação")
+    else console.log("Falhou 12º - Tese subtração")
+
+if (multiplicacao(-4, 2) === -8) console.log("Passou 13º - teste multiplicação")
+    else console.log("Falhou 13º - Tese subtração")
+
+if (multiplicacao(4, 0) === 0) console.log("Passou 14º - teste multiplicação")
+    else console.log("Falhou 14º - Tese subtração")
+
+//..................Teste da divisão.................
+console.log("Teste Divião")
+
+if (divisao(4, 2) === 2) console.log("Passou 11º - teste Divisão")
+    else console.log("Falhou 11º - Tese Divisão")
+
+if (divisao(4, -2) === -2) console.log("Passou 12º - teste Divisão")
+    else console.log("Falhou 12º - Tese Divisão")
+
+if (divisao(-4, 2) === -2) console.log("Passou 13º - teste Divisão")
+    else console.log("Falhou 13º - Tese Divisão")
+
+if (divisao(4, 0) === undefined) console.log("Passou 14º - teste Divisão")
+    else console.log("Falhou 14º - Tese Divisão")
+
+if (divisao(0, 4) === 0) console.log("Passou 15º - teste Divisão")
+    else console.log("Falhou 15º - Tese Divisão")
+
