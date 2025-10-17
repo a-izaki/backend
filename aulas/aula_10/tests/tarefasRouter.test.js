c374a3eec0f143f21d21187fab5948620a6e4451
airton-izaki <airton.izaki@iesb.edu.br>
2025-10-17 10:45:38 -0300

    Commit aula 10

--- aulas/aula_10/tests/tarefasRouter.test.js
+++ aulas/aula_10/tests/tarefasRouter.test.js
@@ -9,6 +9,8 @@ describe('Teste do recurso /tarefas', () => {
         const response = await request.post(url).send( { nome: 'Estudar' } );
         expect(response.status).toBe(201);
         expect(response.body.id).toBeDefined();
+        expect(response.body.nome).toBe('Estudar');
+        expect(response.body.concluida).toBe(false);
         id = response.body.id
     });
 
@@ -24,7 +26,20 @@ describe('Teste do recurso /tarefas', () => {
         expect(response.body.id).toBeDefined();
     });
 
-    test('PUT /id deve retornar 200', async() => {
+    test('GET / id deve retornar 404', async() => {
+        const response = await request.get(`${url}/000000000000000000000000`);
+        expect(response.status).toBe(404);
+        expect(response.body.msg).toBe('Tarefa não encontrada');
+    });
+
+    test('PUT / id deve retornar 404', async() => {
+        const response = await request.get(`${url}/000000000000000000000000`);
+        expect(response.status).toBe(404);
+        expect(response.body.msg).toBe('Tarefa não encontrada');
+    });
+
+    
+    test('PUT /id deve retornar 404', async() => {
         const response = await request
         .put(`${url}/${id}`)
         .send({ nome: "Estudar REST", concluida: true});
@@ -35,6 +50,12 @@ describe('Teste do recurso /tarefas', () => {
     test('DELETE /id deve retornar 204', async() => {
         const response = await request.delete(`${url}/${id}`)
         expect(response.status).toBe(204);
+    });
+
+    test('DELETE / id deve retornar 404', async() => {
+        const response = await request.get(`${url}/000000000000000000000000`);
+        expect(response.status).toBe(404);
+        expect(response.body.msg).toBe('Tarefa não encontrada');
     });
 
 })
